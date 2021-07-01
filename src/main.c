80f1a6b58d167fbdf5974f3440a6cfe106ba1ec7
Adam Salhi <adam06genius@gmail.com>
2021-07-01 16:59:47 +0100

    Finished adding commands

--- src/main.c
+++ src/main.c
@@ -10,9 +10,20 @@ int main(int argc, char *argv[]) {
 	} else if (argc == 2) {
 		if (strcmp(argv[1], "init") == 0 && getcwd(current_dir, sizeof(current_dir)) != NULL) {
 			printf("Initializing new project in directory %s\n", current_dir);
+		} else if (strcmp(argv[1], "new") == 0) {
+			printf("Error: Command 'new' requires extra parameters.\n");
+		} else if (strcmp(argv[1], "del") == 0) {
+			printf("Error: Command 'del' requires extra parameters.\n");
 		} else {
 			printf("Error: Unknown argument\n");
 		}
-	} 
+	} else if (argc == 3) {
+		if (strcmp(argv[1], "new") == 0 && getcwd(current_dir, sizeof(current_dir)) != NULL) {
+			printf("+ Creating new directory '%s' in %s\n", argv[2], current_dir);
+		} else if (strcmp(argv[1], "del") == 0 && getcwd(current_dir, sizeof(current_dir)) != NULL) {
+			printf("+ Deleting project '%s'\n", argv[2]);
+		}
+	}
+
 	return 0;
 }
